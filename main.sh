d8a51ab9fd1e5e4ff0034e5eb64eb750850ca493
hayashi-y-br <hayashi.y.br@m.titech.ac.jp>
2024-09-25 16:50:07 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #$ -cwd
 #$ -l gpu_h=1
-#$ -l h_rt=4:00:00
+#$ -l h_rt=12:00:00
+#$ -N 012-0
 
 eval "$(/apps/t4/rhel9/free/miniconda/24.1.2/bin/conda shell.bash hook)"
 conda activate mil
 
-python main.py -m
-# python main.py -m settings.batch_size=8,16,32 settings.lr=0.001,0.0001,0.00001 settings.reg=0.001,0.0001,0.00001 seed='range(3)' 
+python main.py -m seed=0 lr=0.001,0.0005,0.0001,0.00005,0.00001 reg=0.001,0.0005,0.0001,0.00005,0.00001
