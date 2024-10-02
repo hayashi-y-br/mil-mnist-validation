da497cc0f364a673b8e7caff9a04bd49a3b52c9f
hayashi-y-br <hayashi.y.br@m.titech.ac.jp>
2024-10-02 16:38:46 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,9 +2,8 @@
 #$ -cwd
 #$ -l gpu_h=1
 #$ -l h_rt=12:00:00
-#$ -t 1-5
 
 eval "$(/apps/t4/rhel9/free/miniconda/24.1.2/bin/conda shell.bash hook)"
 conda activate mil
 
-python main.py -m seed=$(($SGE_TASK_ID-1)) lr=0.001,0.0005,0.0001,0.00005,0.00001 reg=0.001,0.0005,0.0001,0.00005,0.00001
+python main.py -m model=additive,attention dataset=config-012,config-169,config-348,config-750,config-926
